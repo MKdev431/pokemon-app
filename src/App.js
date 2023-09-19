7c360fb3509159cd99345556bfcba5a0c5ae468f
Michal Kurek (Mike) <michal@kurek.pl>
2023-09-19 22:19:02 +0200

    add pagination buttons

--- src/App.js
+++ src/App.js
@@ -2,17 +2,47 @@ import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
 import axios from "axios";
 import "./App.scss";
+import Pagination from "./Pagination";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20").then(res => {
+    setLoading(true);
+    let cancel;
+    axios.get(currentPageUrl, { cancelToken: new axios.CancelToken(c => (cancel = c)) }).then(res => {
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
       setPokemon(res.data.results.map(p => p.name));
     });
-  }, []);
 
-  return <PokemonList pokemon={pokemon} />;
+    return () => cancel();
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function gotoPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
--- src/Pagination.js
+++ src/Pagination.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function Pagination() {
-  return <div></div>;
+export default function Pagination({ gotoPrevPage, gotoNextPage }) {
+  return (
+    <>
+      {gotoPrevPage && <button onClick={gotoPrevPage}>Prev</button>}
+      {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+    </>
+  );
 }
